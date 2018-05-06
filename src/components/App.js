d0eee1bc244c5c02f8f4832aa5f279bd0b19a0d2
Chance Burke <burke@Chances-MacBook-Pro.local>
2018-05-06 16:30:29 -0600

    setting up scaffolding

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/app_style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Fuck you
+      </div>
+    );
+  }
+}
+
+export default App;
